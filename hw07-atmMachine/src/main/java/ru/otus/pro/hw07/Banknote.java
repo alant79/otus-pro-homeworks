151227d0766500ff319548a265cde425518abe95
Антон <a.alexeyev@mail.ru>
2023-10-24 01:10:58 +0300

    homework7 done

---
+++ hw07-atmMachine/src/main/java/ru/otus/pro/hw07/Banknote.java
@@ -0,0 +1,28 @@
+package ru.otus.pro.hw07;
+
+public class Banknote {
+
+    private int value;
+    private Currency currency;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public Banknote(int value, Currency currency) {
+        this.value = value;
+        this.currency = currency;
+    }
+}
---
+++ hw07-atmMachine/src/main/java/ru/otus/pro/hw07/moneyBox/MoneyBoxImpl.java
@@ -0,0 +1,44 @@
+package ru.otus.pro.hw07.moneyBox;
+
+import ru.otus.pro.hw07.Banknote;
+
+public class MoneyBoxImpl implements MoneyBox{
+    private Banknote typeOfBanknote;
+    private int quantity;
+    private int capacity;
+
+    public MoneyBoxImpl(Banknote typeOfBanknote, int capacity ) {
+        this.typeOfBanknote = typeOfBanknote;
+        this.capacity = capacity;
+    }
+    @Override
+    public void putToMoneyBox(int quantity) {
+        if (quantity + this.quantity > capacity) {
+            throw new RuntimeException("Moneybox is full");
+        }
+        this.quantity += quantity;
+    }
+    @Override
+    public void getFromMoneyBox(int quantity) {
+        if (this.quantity < quantity) {
+            throw new RuntimeException("Not enough money in moneybox");
+        }
+        this.quantity -= quantity;
+    }
+    @Override
+    public Banknote getTypeOfBanknote() {
+        return typeOfBanknote;
+    }
+    @Override
+    public int getTotalValue() {
+        return quantity * typeOfBanknote.getValue();
+    }
+
+    public int getQuantity() {return quantity; }
+
+    @Override
+    public int getFreePlace() {
+        return this.capacity - this.quantity;
+    }
+
+}
